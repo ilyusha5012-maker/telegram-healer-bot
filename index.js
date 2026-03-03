0eafbb97178d6974831582402e43052a41a7117e
ilyusha5012-maker <ilyusha5012@gmail.com>
2026-03-03 22:26:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+const url = process.env.RENDER_EXTERNAL_URL;
+
+const app = express();
+app.use(express.json());
+
+const bot = new TelegramBot(token);
+
+bot.setWebHook(`${url}/bot${token}`);
+
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "Привет 🙌 Это твой healer-бот. Скоро здесь будет система диагностики."
+  );
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
